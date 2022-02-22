58a31d065170e70bb6f325af7e35c30d5ae587e6
Dan Solo <dansolo.cc@gmail.com>
2022-02-21 21:24:16 -0600

    Add Custom app bar to details screen

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -5,9 +5,47 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //TODO: Cambiar por una instancia de Movie
+    final String movie =
+        ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+
     return const Scaffold(
-      body: Center(
-        child: Text('DetailsScreen'),
+      body: CustomScrollView(
+        slivers: [
+          _CusomAppBar(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CusomAppBar extends StatelessWidget {
+  const _CusomAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: const EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          child: Text(
+            'movie.title',
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+        background: FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }

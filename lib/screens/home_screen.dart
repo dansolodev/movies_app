554fd3c9d1610697754ebfbc06a76f8b1c5b594b
Daniel Cinto Conde <dansolo@MacBook-Pro-de-Dan-Solo.local>
2022-03-03 22:31:26 -0600

    Add infinite scroll horizontal

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -28,6 +28,7 @@ class HomeScreen extends StatelessWidget {
             MovieSlider(
               movies: moviesProvider.popularMovies,
               title: "Populares",
+              onNextPage: () => moviesProvider.getPopularMovies(),
             ),
           ],
         ),

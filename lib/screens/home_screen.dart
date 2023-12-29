7cbd14a7b833dd153eebe115035aae1686623a73
dansolodev <dansolo.cc@gmail.com>
2023-12-29 11:26:22 -0600

    Add movie slider

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -16,10 +16,13 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          CardSwiper(),
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            CardSwiper(),
+            MovieSlider(),
+          ],
+        ),
       ),
     );
   }
